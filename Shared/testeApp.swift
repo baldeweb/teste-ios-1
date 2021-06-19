43caf11365c27ae537e366eaf23cf829d7976503
wallace <wallacecorin@gmail.com>
2021-06-19 12:31:09 -0300

    first commit

---
+++ Shared/testeApp.swift
@@ -0,0 +1,17 @@
+//
+//  testeApp.swift
+//  Shared
+//
+//  Created by Wallace Baldenebre on 06/04/21.
+//
+
+import SwiftUI
+
+@main
+struct testeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
